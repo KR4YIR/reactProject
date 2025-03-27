fdf356f40b807aae21f484e39a1fd431475f5335
kr4yir <ibr4himbayram@gmail.com>
2025-03-27 16:53:32 +0300

    bone

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,20 @@
-import { useEffect, useState } from "react";
-import Header from "./Header";
-import InitMap from "./initMap";
-import displayObj from "./displayObj";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllObjects } from "./redux/objectSlice";
-import {ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import ConfirmPanel from "../public/ConfirmPanel";
-import { StopEditButton } from "./StopEditButton";
+import { Route, Routes } from "react-router-dom";
+import NotFoundPage from './pages/NotFoundPage'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Map from "./Map";
 function App() {
-  const [isConfirmPanelOpen, setIsConfirmPanelOpen] = useState(false);
-  const dispatch = useDispatch();
-  const { objects } = useSelector(state => state.object);
   
-  // Fetch objects on component mount
-  useEffect(() => {
-    dispatch(getAllObjects());
-  }, [dispatch]);
-  
-  // Call displayObj whenever objects change
-  useEffect(() => {
-    if (objects && objects.length > 0) {
-      displayObj(objects);
-    }
-  }, [objects]);
   
   return(
     <>
-      <ToastContainer position="top-right" autoClose={3000} />
-      <Header />
-      <StopEditButton/>
-      <InitMap/>
-      <ConfirmPanel
-        isOpen={isConfirmPanelOpen}
-        onClose={() => setIsConfirmPanelOpen(false)} 
-      /> 
+       
+      <Routes>
+        <Route path="/" element={<Map/>}/>
+        <Route path="/signIn" element={<SignIn/>}/>
+        <Route path="/signUp" element={<SignUp/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/Map.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Header from "./Header";
+import InitMap from "./initMap";
+import displayObj from "./displayObj";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllObjects } from "./redux/objectSlice";
+import {ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ConfirmPanel from "../public/ConfirmPanel";
+import { StopEditButton } from "./StopEditButton";
+
+const Map = () => {
+    const [isConfirmPanelOpen, setIsConfirmPanelOpen] = useState(false);
+  const dispatch = useDispatch();
+  const { objects } = useSelector(state => state.object);
+  
+  // Fetch objects on component mount
+  useEffect(() => {
+    dispatch(getAllObjects());
+  }, [dispatch]);
+  
+  // Call displayObj whenever objects change
+  useEffect(() => {
+    if (objects && objects.length > 0) {
+      displayObj(objects);
+    }
+  }, [objects]);
+  return(
+    <>
+        <ToastContainer position="top-right" autoClose={3000} />
+        <Header />
+        <StopEditButton/>
+        <InitMap/>
+        <ConfirmPanel
+            isOpen={isConfirmPanelOpen}
+            onClose={() => setIsConfirmPanelOpen(false)} 
+        />
+    </>
+  )
+}
+export default Map;
